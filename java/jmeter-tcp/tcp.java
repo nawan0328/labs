43674085c3d82d621a3a92685f705a8ed95e4845
nawan <nawan0328@gmail.com>
2019-12-24 10:27:28 +0900

    first commit

---
+++ java/jmeter-tcp/tcp.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.*;
+
+class TCPServer
+{
+	public static void main(String argv[]) throws Exception
+	{
+		String fromclient;
+		String toclient;
+		ServerSocket Server = new ServerSocket (1001);
+		System.out.println ("TCPServer Waiting for client on port 1001");
+		
+		while(true)
+		{
+			Socket connected = Server.accept();
+			System.out.println( " THE CLIENT"+" "+ connected.getInetAddress() +":"+connected.getPort()+" IS CONNECTED ");
+			BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));  
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader (connected.getInputStream()));
+			PrintWriter outToClient = new PrintWriter(connected.getOutputStream(),true);
+			
+			while ( true )
+			{
+				toclient = "OK";
+				outToClient.println(toclient);
+				connected.close();
+				break;
+			}
+		}
+	}
+} 
